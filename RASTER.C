5197ae07144ccacfa37ce85c90a3328596000bd6
johngalang1 <113706696+johngalang1@users.noreply.github.com>
2024-10-02 13:25:59 -0600

    Update RASTER.C

--- RASTER.C
+++ RASTER.C
@@ -1,6 +1,28 @@
 #include "raster.h"
 
-void plot_something_1(UINT16 *base, int x, int y, ...)
-{
-	/* [TO DO] delete this example function */
+void plot_apple_32(UINT32 *base, int x, int y, 
+                    const UINT32 *bitmap, unsigned int height)
+{ 
+    UINT32 *next = base + (y * 20) + ( x >> 5); 
+    int i = 0;      
+
+    while(i < height)
+    { 
+        *next = bitmap[i]; 
+        next += 20; 
+        i += 1;
+    } 
+} 
+
+void plot_basket_64(UINT32 *base, int x, int y, 
+                    const UINT32 *bitmap[BASKET_HEIGHT][2], unsigned int height)
+{ 
+    int i;
+    UINT32 *next;
+    
+    for (i = 0; i < height; i++) {
+        next = base + (y + i) * (SCREEN_WIDTH >> 5) + (x >> 5);
+        *next = bitmap[i][0];
+        *(next + 1) = bitmap[i][1];
+    }
 }
