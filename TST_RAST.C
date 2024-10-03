8d5c705b90fe60e4e5b184aa9a9015feddeaa135
zach <113077228+zlefl221@users.noreply.github.com>
2024-10-03 11:28:51 -0600

    Update TST_RAST.C

--- TST_RAST.C
+++ TST_RAST.C
@@ -7,6 +7,8 @@ int main()
 	UINT32 *FB = Physbase();  
 	plot_apple_32(FB, 400, 100, apple_bitmap, APPLE_HEIGHT); 
 	plot_basket_64(FB, 400, 200, basket_bitmap, BASKET_HEIGHT);
+	plot_vertical_line(FB, 100, 0, 400);
+	plot_vertical_line(FB, 200, 100, 300);
 
 	return 0;
 }
