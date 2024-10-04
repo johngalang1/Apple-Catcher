2d9678366c3f356b1d026f3f9d4e7ec9459ca904
johngalang1 <113706696+johngalang1@users.noreply.github.com>
2024-10-04 11:24:05 -0600

    Update TST_RAST.C

--- TST_RAST.C
+++ TST_RAST.C
@@ -4,11 +4,11 @@
 
 int main()
 {
-	UINT32 *FB = Physbase();  
-	plot_apple_32(FB, 400, 100, apple_bitmap, APPLE_HEIGHT); 
-	plot_basket_64(FB, 400, 200, basket_bitmap, BASKET_HEIGHT);
-	plot_vertical_line(FB, 100, 0, 400);
-	plot_vertical_line(FB, 200, 100, 300);
+	UINT32 *FB32 = Physbase();  
+	UINT16 *FB16 = Physbase();
+	plot_apple_32(FB32, 400, 100, apple_bitmap, APPLE_HEIGHT); 
+	plot_basket_64(FB32, 400, 150, basket_bitmap, BASKET_HEIGHT);
+	plot_char(FB16, 200, 200, letterA_bitmap, CHARACTER_HEIGHT);
 
 	return 0;
 }
