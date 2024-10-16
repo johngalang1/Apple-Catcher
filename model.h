84d5dd7911d17ff7ec1c79b57e4f98bc748a516e
johngalang1 <113706696+johngalang1@users.noreply.github.com>
2024-10-15 19:21:02 -0600

    Update model.h

--- model.h
+++ model.h
@@ -1,13 +1,54 @@
 #ifndef model.h
 #define model.h
+#include "types.h"
 
 typedef struct 
 {
-unsigned int x, y;
-unsigend int width = 32;
-unsigend int height = 32;
-}apple;
+    UINT16 x, y;
+    UINT32 width;
+    UINT32 height;
+} apple;
 
+typedef struct
+{ 
+    UINT16 x, y; 
+    int delta_x;
+    UINT32 width;
+    UINT32 height;
+} basket; 
+
+typedef struct {
+    UINT16 digit1_x, digit1_y;  
+    UINT16 digit2_x, digit2_y;  
+    UINT16 width;               
+    UINT32 height;              
+    int value;                  
+    const UINT16 *digit_bitmaps[10];  
+} score;
+
+typedef struct {
+    UINT16 digit1_x, digit1_y;  
+    UINT16 digit2_x, digit2_y;  
+    UINT16 width;               
+    UINT32 height;              
+    int value;                  
+    const UINT16 *digit_bitmaps[10];  
+} timer_round;
+
+typedef struct {
+    UINT16 x, y;               
+    UINT16 width;              
+    UINT32 height;             
+    int value;                 
+    const UINT32 *digit_bitmaps[5];  
+} timer_start;
+
+
+typedef struct {
+    UINT16 letter_x[NUM_LETTERS];   
+    UINT16 letter_y[NUM_LETTERS];   
+    const UINT16 *digit_bitmaps[NUM_LETTERS];  
+} message;
 
 
 #endif
