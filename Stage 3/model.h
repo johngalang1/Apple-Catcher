6f091c2e314e1e7f9be746510273148cafb09cd0
johngalang1 <113706696+johngalang1@users.noreply.github.com>
2024-10-16 18:04:03 -0600

    Update model.h

--- Stage 3/model.h
+++ Stage 3/model.h
@@ -13,27 +13,27 @@ typedef struct
 typedef struct
 { 
     UINT16 x, y;        /*Position of Basket*/
-    int delta_x;        /*Horizontal Velocity*/
+    int delta_x;        /*Horizontal displacement per clock tick*/
     UINT32 width;
     UINT32 height;
 } basket; 
 
 typedef struct {
-    UINT16 digit1_x, digit1_y;  
-    UINT16 digit2_x, digit2_y;  
+    UINT16 digit1_x, digit1_y;         /* Fixed position for digit 1 */
+    UINT16 digit2_x, digit2_y;         /* Fixed position for digit 2 */
     UINT16 width;               
-    UINT32 height;              
+    UINT32 height;                     /* Score value (integer < 60) */
     int value;                  
-    const UINT16 *digit_bitmaps[10];  
+    const UINT16 *digit_bitmaps[10];   /* Array of bitmaps for digits 0-9 */
 } score;
 
 typedef struct {
-    UINT16 digit1_x, digit1_y;  
-    UINT16 digit2_x, digit2_y;  
+    UINT16 digit1_x, digit1_y;         /* Fixed position for digit 1 */
+    UINT16 digit2_x, digit2_y;         /* Fixed position for digit 2 */
     UINT16 width;               
     UINT32 height;              
-    int value;                  
-    const UINT16 *digit_bitmaps[10];  
+    int value;                         /* Timer value (integer <= 60) */
+    const UINT16 *digit_bitmaps[10];   /* Array of bitmaps for digits 0-9 */
 } timer_round;
 
 typedef struct {
@@ -41,14 +41,14 @@ typedef struct {
     UINT16 width;              
     UINT32 height;             
     int value;                 
-    const UINT32 *digit_bitmaps[5];  
+    const UINT32 *digit_bitmaps[5];  /* Array of bitmaps for digits 5-1 */
 } timer_start;
 
 
 typedef struct {
-    UINT16 letter_x[NUM_LETTERS];   
-    UINT16 letter_y[NUM_LETTERS];   
-    const UINT16 *digit_bitmaps[NUM_LETTERS];  
+    UINT16 letter_x[NUM_LETTERS];   /* Array of x positions for each letter */
+    UINT16 letter_y[NUM_LETTERS];   /* Array of y positions for each letter */
+    const UINT16 *digit_bitmaps[NUM_LETTERS];  /* Array of bitmaps, one for each character in the message */
 } message;
 
 
