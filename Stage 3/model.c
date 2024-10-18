1e0945458dcd6001c45b5da6b1fa5799f5d77007
zach <113077228+zlefl221@users.noreply.github.com>
2024-10-18 10:39:37 -0600

    Update model.c

--- Stage 3/model.c
+++ Stage 3/model.c
@@ -50,3 +50,35 @@ void update_score(UINT16 *base, score *curr_score)
     clear_char(base, curr_score->digit2_x, curr_score->digit2_y, CHARACTER_HEIGHT);
     plot_char(base, curr_score->digit2_x, curr_score->digit2_y, num_maps[digit_2], CHARACTER_HEIGHT);
 }
+
+/* round timer functions */
+
+timer_round *init_round_timer()
+{
+    static timer_round t;
+        t.digit1_x = 608; /* ones digit */
+        t.digit1_y = 16;
+        t.digit2_x = 592; /* tens digit */
+        t.digit2_y = 16;
+        t.height = CHARACTER_HEIGHT;
+        t.value = 60; 
+    return &t;
+}
+
+void decrement_round_timer(UINT16 *base, timer_round *t)
+{
+    t->value -= 1;
+    update_round_timer(base, t);
+}
+
+void update_round_timer(UINT16 *base, timer_round *t)
+{
+    int digit_1, digit_2;   /* digit 1 is ones digit, digit 2 is tens digit */
+    digit_1 = t->value % 10;
+    digit_2 = t->value / 10;
+
+    clear_char(base, t->digit1_x, t->digit1_y, CHARACTER_HEIGHT);
+    plot_char(base, t->digit1_x, t->digit1_y, num_maps[digit_1], CHARACTER_HEIGHT);
+    clear_char(base, t->digit2_x, t->digit2_y, CHARACTER_HEIGHT);
+    plot_char(base, t->digit2_x, t->digit2_y, num_maps[digit_2], CHARACTER_HEIGHT);
+}
