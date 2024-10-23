4a95a186cf2ba39b7ce33914767bf0857809e28f
johngalang1 <113706696+johngalang1@users.noreply.github.com>
2024-10-22 21:06:59 -0600

    Add files via upload

---
+++ Stage 3/events.h
@@ -0,0 +1,25 @@
+#ifndef EVENTS_H
+#define EVENTS_H
+#include "types.h"
+#include "RASTER.H" 
+#include "model.h"
+
+
+void check_basket_collision(basket *b); 
+
+int check_apple_collision(basket *b, apple *a);
+
+void reset_apple(apple *a); 
+
+void update_score(UINT16 *base, score *curr_score); 
+
+void update_round_timer(UINT16 *base, timer_round *t);  
+
+timer_start *init_start_timer();
+int is_start_time_up(timer_start *t);
+
+/* Message Functions */ 
+void display_message(message *msg);
+void clear_message(message *msg);
+
+#endif
