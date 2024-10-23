299f37e6b68f89dcbf5af5dbfcf0a4a805ccc3b7
zach <113077228+zlefl221@users.noreply.github.com>
2024-10-23 09:39:04 -0600

    Update model.c

--- Stage 3/model.c
+++ Stage 3/model.c
@@ -51,6 +51,18 @@ void increment_score(UINT16 *base, score *curr_score)
     update_score(base, curr_score);
 }
 
+void update_score(UINT16 *base, score *curr_score)
+{
+    int digit_1, digit_2; /*digit 1 is ones digit, digit 2 is tens digit*/
+    digit_1 = curr_score->value % 10;
+    digit_2 = curr_score->value / 10;
+
+    clear_char(base, curr_score->digit1_x, curr_score->digit1_y, CHARACTER_HEIGHT);
+    plot_char(base, curr_score->digit1_x, curr_score->digit1_y, num_maps[digit_1], CHARACTER_HEIGHT);
+    clear_char(base, curr_score->digit2_x, curr_score->digit2_y, CHARACTER_HEIGHT);
+    plot_char(base, curr_score->digit2_x, curr_score->digit2_y, num_maps[digit_2], CHARACTER_HEIGHT);
+}
+
 /* round timer functions */
 
 timer_round *init_round_timer()
