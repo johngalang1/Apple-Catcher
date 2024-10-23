e82f109bffff3477fbc42af5ed5efa9b4e9ba029
zach <113077228+zlefl221@users.noreply.github.com>
2024-10-23 10:25:46 -0600

    Update events.h

--- Stage 3/events.h
+++ Stage 3/events.h
@@ -12,8 +12,10 @@ int check_apple_collision(basket *b, apple *a);
 void reset_apple(apple *a); 
 
 void update_score(UINT16 *base, score *curr_score); 
+void increment_score(UINT16 *base, score *curr_score);
 
-void update_round_timer(UINT16 *base, timer_round *t);  
+void update_round_timer(UINT16 *base, timer_round *t); 
+void decrement_start_timer(timer_start *t);
 
 timer_start *init_start_timer();
 int is_start_time_up(timer_start *t);
