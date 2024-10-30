fee46fc9ac8161f5b7554d5f2972e70eb623c2a6
zach <113077228+zlefl221@users.noreply.github.com>
2024-10-29 20:15:24 -0600

    Add files via upload

---
+++ stage5/stage4.c
@@ -0,0 +1,58 @@
+#include <osbind.h>
+#include "raster.h"
+#include "bitmaps.h"
+#include "model.h"
+#include "events.h"
+#include "types.h"
+#include <stdio.h>
+#include "renderer.h"
+#include "driver.h" 
+
+/* 
+AUTHORS: John G, Zach L
+FILE NAME: stage4.c
+PURPOSE: CONTAINS RENDER TESTING FOR STAGE 4
+*/
+
+int main()
+{
+    UINT32 *FB32 = Physbase();  
+    UINT16 *FB16 = (UINT16*) Physbase();  
+    char input;
+    model *curr_model = init_model();
+    clear_screen(FB32);
+
+    while ( input != 'Q')
+    {
+        printf("\nPress Numbers to Test:\n 1. Basket Movement\n 2. Apple movement\n 3. score rendering\n"
+       " 4. a single frame render(press a key to end)\n 5. master render including countdown timer(press a key to count down)\n Q - Quit\n");
+        input = Cnecin();
+
+        switch (input)
+        {
+            case '1':
+                test_basket_render(FB32, FB16, &(curr_model->b));  
+                break;
+
+            case '2':
+                test_apple_render(FB32, FB16, &(curr_model->apples[0]), &(curr_model->b)); 
+                break;
+
+            case '3':
+                test_score_render(FB32, FB16, &(curr_model->apples[0]), &(curr_model->b), &(curr_model->curr_score));  
+                break;
+            
+            case '4':
+                test_frame_render(FB32, FB16, curr_model); 
+                break;
+
+            case '5':
+                test_master_render(FB32, FB16, &(curr_model->apples[0]), &(curr_model->b),
+                        &(curr_model->curr_score), &(curr_model->rt), &(curr_model->st)); 
+                break;
+            default:
+                break;
+        }
+    }
+    return 0;
+}
