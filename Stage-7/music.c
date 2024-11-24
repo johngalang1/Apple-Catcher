655defcd752e2bced8f72c8b38706b8e9d66e236
johngalang1 <113706696+johngalang1@users.noreply.github.com>
2024-11-24 00:48:59 -0700

    Update music.c

--- Stage-7/music.c
+++ Stage-7/music.c
@@ -13,34 +13,108 @@ typedef struct {
 } Note;
 
 static Note melody[] = {
-    {478, 35}, /* C, 0.5 seconds */
-    {379, 35}, /* E, 0.5 seconds */
-    {319, 35}, /* G, 0.5 seconds */
-    {284, 35}, /* B, 0.5 second */
-    {319, 35}, /* G, 0.5 seconds */
-    {379, 35}, /* E, 0.5 seconds */
-    {478, 35}, /* C, 0.5 seconds */ 
-    {379, 35}, /* E, 0.5 seconds */
-    {319, 35}, /* G, 0.5 seconds */
-    {284, 35}, /* B, 0.5 second */
-    {319, 35}, /* G, 0.5 seconds */
-    {379, 35}, /* E, 0.5 seconds */
-    {426, 35}, /* D, 0.5 seconds */
-    {358, 35}, /* F, 0.5 seconds */
-    {284, 35}, /* A, 0.5 seconds */
-    {253, 35}, /* B, 0.5 seconds */
-    {284, 35}, /* A, 0.5 seconds */
-    {358, 35}, /* F, 0.5 seconds */
-    {426, 35}, /* D, 0.5 seconds */
-    {358, 35}, /* F, 0.5 seconds */
-    {284, 35}, /* A, 0.5 seconds */
-    {253, 35}, /* B, 0.5 seconds */
-    {284, 35}, /* A, 0.5 seconds */
-    {358, 35}, /* F, 0.5 seconds */
+    {451 / 2, 26}, /* C# */ 
+    {0, 2},  
+    {268 / 2, 18}, /* A# */
+    {0, 2},  
+    {301 / 2, 26}, /* G# */ 
+    {0, 2},  
+    {268 / 2, 18}, /* A# */
+    {0, 2},  
+    {301 / 2, 26}, /* G# */ 
+    {0, 2}, 
+
+    {451 / 2, 26}, /* C# */ 
+    {0, 2},  
+    {268 / 2, 18}, /* A# */
+    {0, 2},  
+    {301 / 2, 26}, /* G# */ 
+    {0, 2},  
+    {268 / 2, 18}, /* A# */
+    {0, 2},  
+    {301 / 2, 26}, /* G# */ 
+    {0, 2}, 
+
+    {268 / 2, 16}, /* A# */
+    {301 / 2, 16}, /* G# */
+    {358 / 2, 24}, /* F */
+    {0, 4},  
+    {358 / 2, 28}, /* F */
+    {402 / 2, 20}, /* D# */
+    {358 / 2, 96}, /* F */
+
+    /* //////// */
+
+    {451 / 2, 26}, /* C# */ 
+    {0, 2},  
+    {268 / 2, 18}, /* A# */
+    {0, 2},  
+    {301 / 2, 26}, /* G# */ 
+    {0, 2},  
+    {268 / 2, 18}, /* A# */
+    {0, 2},  
+    {301 / 2, 26}, /* G# */ 
+    {0, 2}, 
+
+    {451 / 2, 26}, /* C# */ 
+    {0, 2},  
+    {268 / 2, 18}, /* A# */
+    {0, 2},  
+    {301 / 2, 26}, /* G# */ 
+    {0, 2},  
+    {268 / 2, 18}, /* A# */
+    {0, 2},  
+    {301 / 2, 26}, /* G# */ 
+    {0, 2}, 
+
+    {0, 2},   
+    {301 / 2, 26}, /* G# */ 
+    {0, 2},   
+    {268 / 2, 26}, /* A# */ 
+    {0, 2},   
+    {253 / 2, 16}, /* B */ 
+    {0, 2},   
+    {268 / 2, 24}, /* A# */ 
+    {0, 2},   
+    {301 / 2, 18}, /* G# */ 
+    {0, 2},   
+    {358 / 2, 82}, /* F */ 
+    {0, 2},   
 };
+
+
+static Note melody_b[] = {
+    {451 * 2, 62},  /* C# - Base octave */
+    {451, 62},      /* C# - One octave higher */
+
+    {451 * 2, 62},  /* C# - Base octave */
+    {451, 62},      /* C# - One octave higher */
+
+    {358 * 2, 200}, /* F - Base octave */
+    {0, 4},        
+
+    {451 * 2, 62},  /* C# - Base octave */
+    {451, 62},      /* C# - One octave higher */
+
+    {451 * 2, 62},  /* C# - Base octave */
+    {451, 62},      /* C# - One octave higher */
+
+    {358 * 2, 100}, /* F - Base octave */  
+    {0, 10},       
+    {358 * 2, 45}, /* F - Base octave */   
+    {0, 2},        
+    {358 * 2, 45}, /* F - Base octave */  
+    {0, 2},     
+};
+
+
 
 /* Number of notes in the melody */
 #define MELODY_LENGTH (sizeof(melody) / sizeof(Note))
+#define MELODY_B_LENGTH (sizeof(melody_b) / sizeof(Note)) 
+
+static int current_note_index_b = 0;   /* Index of the current note for Channel B */
+static int remaining_duration_b = 0;  /* Time left for the current note on Channel B */
  
 /* Variables to keep track of the melody's state */
 static int current_note_index = 0;    /* Index of the current note */
@@ -54,15 +128,16 @@ DETAILS:
     - Resets the melody to the beginning by setting the `current_note_index` to 0.
     - Loads the duration of the first note into the `remaining_duration` variable.
     - If the first note has a non-zero pitch:
-        - Configures Channel A with the note's pitch using `set_tone`.
-        - Sets the volume of Channel A to medium using `set_volume`.
+        - Configures the note's pitch using `set_tone`.
+        - Sets the volume using `set_volume`.
         - Enables tone generation on Channel A using `enable_channel`.
     - If the first note is a rest (pitch = 0):
-        - Mutes Channel A by setting its volume to 0.
+        - Mutes the Channel by setting its volume to 0.
 */
 void start_music()
 {
-    current_note_index = 0; /* Reset melody to the beginning */
+    /* Initialize Channel A */
+    current_note_index = 0; 
     remaining_duration = melody[current_note_index].duration;
 
     if (melody[current_note_index].pitch > 0) {
@@ -72,7 +147,20 @@ void start_music()
     } else {
         set_volume(0, 0);
     }
+
+    /* Initialize Channel B */
+    current_note_index_b = 0;
+    remaining_duration_b = melody_b[current_note_index_b].duration;
+
+    if (melody_b[current_note_index_b].pitch > 0) {
+        set_tone(1, melody_b[current_note_index_b].pitch); 
+        set_volume(1, 8); 
+        enable_channel(1, 1, 0); 
+    } else {
+        set_volume(1, 0);
+    }
 }
+
 
 /*
 NAME: update_music
@@ -86,22 +174,18 @@ DETAILS:
         - If the end of the melody is reached, wraps back to the first note.
         - Loads the duration of the new note into `remaining_duration`.
         - If the new note has a non-zero pitch:
-            - Configures Channel A with the note's pitch using `set_tone`.
-            - Sets the volume of Channel A using `set_volume`.
-            - Enables tone generation on Channel A using `enable_channel`.
+            - Configures the note's pitch using `set_tone`.
+            - Sets the volume by using `set_volume`.
+            - Enables tone generation by using `enable_channel`.
         - If the new note is a rest (pitch = 0):
             - Mutes Channel A by setting its volume to 0.
     - Increments the `time_elapsed_melody` counter to keep track of total elapsed time during playback.
 */
 void update_music(UINT32 *time_elapsed_melody)
 {
-    /* Decrement the remaining duration for the current note */
+    /* Update Channel A */
     if (--remaining_duration <= 0) {
-        /* Move to the next note in the melody */
-        current_note_index = current_note_index + 1;
-        if (current_note_index >= MELODY_LENGTH) {
-            current_note_index = 0; /* Wrap back to the beginning of the melody */
-        }
+        current_note_index = (current_note_index + 1) % MELODY_LENGTH;
         remaining_duration = melody[current_note_index].duration;
 
         if (melody[current_note_index].pitch > 0) {
@@ -112,6 +196,21 @@ void update_music(UINT32 *time_elapsed_melody)
             set_volume(0, 0);
         }
     }
+
+    /* Update Channel B */
+    if (--remaining_duration_b <= 0) {
+        current_note_index_b = (current_note_index_b + 1) % MELODY_B_LENGTH;
+        remaining_duration_b = melody_b[current_note_index_b].duration;
+
+        if (melody_b[current_note_index_b].pitch > 0) {
+            set_tone(1, melody_b[current_note_index_b].pitch); 
+            set_volume(1, 8);
+            enable_channel(1, 1, 0); 
+        } else {
+            set_volume(1, 0);
+        }
+    }
+
     /* Increment the elapsed time counter */
     (*time_elapsed_melody)++;
 }
