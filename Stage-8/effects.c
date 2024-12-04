c09d8828885ec8d08207ae13b1cf0912c3c517cb
johngalang1 <113706696+johngalang1@users.noreply.github.com>
2024-12-04 13:01:34 -0700

    Update effects.c

--- Stage-8/effects.c
+++ Stage-8/effects.c
@@ -14,9 +14,6 @@ PARAMETERS: None
 PURPOSE: Plays a sound effect when a point is gained.
 */
 void play_score() {
-    int i;
-
-    /* Set up the sound */
     set_tone(2, 198);  
     set_volume(2, 12);         
     enable_channel(2, 1, 0);    
@@ -30,7 +27,6 @@ PURPOSE: Plays a sound effect when the apple hits the bottom of the screen.
 */
 
 void play_splat() {
-    int i;   
     set_tone(2, 451);        
     set_noise(12);
     set_volume(2, 12);        
